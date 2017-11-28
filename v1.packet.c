a8c0f525b36515d4e6f56e61fad10ed25a9fb253
Nat Weerawan <nat@cmmc.io>
2017-11-28 16:48:30 +0700

    Create v1.packet.c

---
+++ v1.packet.c
@@ -0,0 +1,28 @@
+typedef struct __attribute((__packed__)) {
+  uint8_t from[6];
+  uint8_t to[6];
+  uint8_t type = 1;
+  uint32_t battery = 0x00;
+  uint32_t field1 = 0x00;
+  uint32_t field2 = 0x00;
+  uint32_t field3 = 0x00;   
+  uint32_t field4 = 0x00;
+  uint32_t field5 = 0x00;
+  uint32_t field6 = 0x00;
+  uint8_t nameLen;
+  char deviceName[15];
+  uint32_t ms = 0;
+  uint32_t sum;
+} CMMC_SENSOR_T;
+
+typedef struct __attribute((__packed__)) {
+  uint8_t header[2] = {0x7e, 0x7f};
+  uint8_t project = 1;
+  uint32_t reserved = 0xffffffff;
+  CMMC_SENSOR_T data;
+  uint32_t sleepTime;
+  uint32_t ms;
+  uint32_t sum;
+  uint8_t tail[2] = {0x0d, 0x0a};
+} CMMC_PACKET_T;
+
